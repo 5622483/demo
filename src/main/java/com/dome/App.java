c1f9eca5b3668d77d6ec60810a5a236fb4a78b2e
Anonymity <5622483@qq.com>
2021-06-07 18:26:45 +0800

    demo

---
+++ src/main/java/com/dome/App.java
@@ -0,0 +1,18 @@
+package com.dome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName App
+ * @Author Anonymity
+ * @CreateTime 2021年06月07日 18:25:06
+ * @Description TODO
+ * @Version 1.0.0
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
