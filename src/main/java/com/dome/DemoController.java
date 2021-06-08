8c032bc040705c852971ba2eb3424f270fb0bf30
Anonymity <5622483@qq.com>
2021-06-08 16:50:39 +0800

    demo

--- src/main/java/com/dome/DemoController.java
+++ src/main/java/com/dome/DemoController.java
@@ -14,6 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @RequestMapping("/hello")
     public String hello() {
-        return "HelloWord!!!" ;
+        return "HelloWord!!!";
     }
 }
